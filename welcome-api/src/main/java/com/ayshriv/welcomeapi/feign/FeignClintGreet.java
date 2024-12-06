bb887deaf275bd13514fd9dcc217ae7dc7bde074
ayushstwt <ayusrivastava2k3@gmail.com>
2024-12-06 22:55:18 +0530

    fix/ feign client changes.

--- welcome-api/src/main/java/com/ayshriv/welcomeapi/feign/FeignClintGreet.java
+++ welcome-api/src/main/java/com/ayshriv/welcomeapi/feign/FeignClintGreet.java
@@ -3,7 +3,7 @@ package com.ayshriv.welcomeapi.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "greet-service")
+@FeignClient(name = "GREET-API")
 public interface FeignClintGreet {
 
     @GetMapping("/greet")
