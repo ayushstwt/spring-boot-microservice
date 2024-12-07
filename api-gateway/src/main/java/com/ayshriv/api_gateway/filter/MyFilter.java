6f9e164ebd9f3b97ea9a041ee83b40a620448d7a
ayushstwt <ayusrivastava2k3@gmail.com>
2024-12-08 02:05:16 +0530

    API Gateway is created

---
+++ api-gateway/src/main/java/com/ayshriv/api_gateway/filter/MyFilter.java
@@ -0,0 +1,8 @@
+package com.ayshriv.api_gateway.filter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFilter implements GlobalFilter {
+
+}
--- greet-api/src/main/java/com/ayshriv/greetapi/rest/GreetRestController.java
+++ greet-api/src/main/java/com/ayshriv/greetapi/rest/GreetRestController.java
@@ -1,13 +1,19 @@
 package com.ayshriv.greetapi.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetRestController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/greet")
     public String getGreeting() {
-        return "Hello World";
+        String property = environment.getProperty("server.port");
+        return "Good Evening"+ property;
     }
 }
